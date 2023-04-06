8f623c9514bfdd7c45235804eaad9e744b065752
Himanshu055 <himanshu.sharmav45@gmail.com>
2023-04-06 16:27:49 +0530

    Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'teacher';
+  public questionList: any = [];
+   formGroupArray:FormArray | undefined
+  userPost = this.fb.group({
+    questionText: ['', Validators.required],
+    optList: this.fb.group({
+     a: ['', Validators.required],
+      b: ['', Validators.required],
+      c: ['', Validators.required],
+      d: ['', Validators.required],  
+    
+    }),
+    correct: ['', Validators.required],  
+    explanation:['',Validators.required]
+  });
+
+  constructor(private http: HttpClient, private fb: FormBuilder) { }
+  myForm!: FormGroup;
+
+  ngOnInit(): void {
+
+  }
+
+  onSubmit(data: any) {
+    this.http.post('http://localhost:1337/question', data)
+      .subscribe((result) => {
+        this.questionList = result;
+        console.log("result", result) 
+      })
+  }
 }
